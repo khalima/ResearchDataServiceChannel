eed7ed3efb0e52d3541033e12a928c6a642d6eb9
Lauri Aaltonen <lauri@mysteerio.fi>
2017-10-05 18:00:43 +0300

    Added base for admin form for wizard.

---
+++ public/modules/custom/hy_wizard/src/Form/WizardAdminForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\hy_wizard\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class WizardAdminForm.
+ */
+class WizardAdminForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'hy_wizard.wizardadmin',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'wizard_admin_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('hy_wizard.wizardadmin');
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // Comment to surpress validation.
+    $var = 'foo';
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('hy_wizard.wizardadmin')
+      ->save();
+  }
+
+}
