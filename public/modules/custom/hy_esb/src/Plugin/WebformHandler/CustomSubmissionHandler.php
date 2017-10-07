de0bee56017f8fdf198a92e36c96800794dd1537
Marko Korhonen <marko.korhonen@druid.fi>
2017-10-07 08:31:50 +0300

    Added httpClient

--- public/modules/custom/hy_esb/src/Plugin/WebformHandler/CustomSubmissionHandler.php
+++ public/modules/custom/hy_esb/src/Plugin/WebformHandler/CustomSubmissionHandler.php
@@ -4,6 +4,12 @@ namespace Drupal\hy_esb\Plugin\WebformHandler;
 
 use Drupal\webform\Plugin\WebformHandlerBase;
 use Drupal\webform\WebformSubmissionInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\webform\WebformSubmissionConditionsValidatorInterface;
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Webform submission test handler.
@@ -19,6 +25,45 @@ use Drupal\webform\WebformSubmissionInterface;
  * )
  */
 class CustomSubmissionHandler extends WebformHandlerBase {
+
+  /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    LoggerChannelFactoryInterface $logger_factory,
+    ConfigFactoryInterface $config_factory,
+    EntityTypeManagerInterface $entity_type_manager,
+    WebformSubmissionConditionsValidatorInterface $conditions_validator,
+    ClientInterface $http_client) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $logger_factory, $config_factory, $entity_type_manager, $conditions_validator);
+    $this->httpClient = $http_client;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('logger.factory'),
+      $container->get('config.factory'),
+      $container->get('entity_type.manager'),
+      $container->get('webform_submission.conditions_validator'),
+      $container->get('http_client')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -59,7 +104,7 @@ class CustomSubmissionHandler extends WebformHandlerBase {
    * {@inheritdoc}
    */
   public function preSave(WebformSubmissionInterface $webform_submission) {
-    $this->displayMessage(__FUNCTION__);
+    $this->displayMessage(get_class($this->httpClient));
     // @todo Marko adds field 'status' to webform or delegates it to Tuomas
     // @todo Marko double checks that 'efecte_id' is present. No submissions are
     // supposed to go through without it.
